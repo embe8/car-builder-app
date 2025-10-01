e08d061ab2a5af3bf3bf17b5ee4965e432060063
embe8 <eebon@icloud.com>
2025-10-01 02:44:45 -0700

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import ModelsList from './components/ModelsList';
 import CarBuilder from './components/CarBuilder';
@@ -8,7 +8,19 @@ import Details from './components/Details';
 function App() {
   return (
     <Router>
-      <div className="App">
+      <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+        <div className='container container-fluid'>
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/models">Vehicles</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="container mt-4">
         <Routes>
         <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
