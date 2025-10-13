a87b84247f8a251cf1a20a63da0da9a5f52627b4
embe8 <eebon@icloud.com>
2025-10-12 17:47:26 -0700

    Fix supabase url and keys

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://lafrbhntdzfexqrlokzc.supabase.co' // Replace with your project URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhZnJiaG50ZHpmZXhxcmxva3pjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwMDg0NDgsImV4cCI6MjA3NDU4NDQ0OH0.CLv-W7o7S-zou-YFeuN-bM2buj38d4QDwaaAQdAnR6g' // Replace with your anon public key (not the service role)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
-
